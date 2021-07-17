0c9ab86baf003cb13d95d9d28bfcc82abf1863ae
Dale Stewart <stewartdale073@gmail.com>
2021-07-17 19:29:26 -0400

    Implemented Styles for the Devise Registration Page with Bootstrap 4, modified app/views/blogs/index.html.erb, modified app/views/devise/registrations/edit.html/erb, modified app/views/devise/registrations/new.html.erb, modified app/views/layouts/blog.html.erb, modified app/views/layouts/portfolio.html.erb, modified app/views/shared/_nav.html.erb, modified config/routes.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,21 @@
 module ApplicationHelper
    def login_helper style
      if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path, class: style) +
-      "".html_safe +
-      (link_to "Login", new_user_session_path, class: style) 
-     else 
-        link_to "Logout", destroy_user_session_path, method: :delete, class: style 
-     end 
+       (link_to "Register", new_user_registration_path, class: style) +
+       " ".html_safe +
+       (link_to "Login", new_user_session_path, class: style)
+     else
+       link_to "Logout", destroy_user_session_path, method: :delete, class: style
+     end
    end
 
    def source_helper(layout_name)
-     if session[:source]
+      if session[:source]
         greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
         content_tag(:p, greeting, class: "source-greeting")
-     end
-  end
+      end
+    end
+  
 
   def copyright_generator
      CameronViewTool::Renderer.copyright 'Dale Stewart', 'All rights reserved'
