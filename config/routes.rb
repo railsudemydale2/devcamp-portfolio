ec4b7982f747e965d42f4e3e47d7595e6e1dae20
Dale Stewart <stewartdale073@gmail.com>
2021-07-16 21:26:56 -0400

    Modified app/views/pages/about.html.erb, app/views/pages/contact.html.erb, app/views/pages/home.html.erb, app/views/shared/_application_nav.html.erb and modified config/routes.rb and updated styles and finally added map for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'angular-items', to: 'portfolios#angular', as: 'portfolio_angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
   
-  get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :blogs
 
