f60dbbb029a06e39cacb080e2d7e69eafbcc3581
Dale Stewart <stewartdale073@gmail.com>
2021-07-18 01:02:38 -0400

    Updated auth to include draft and published status toggle controls

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,12 @@
 class BlogsController < ApplicationController
   before_action :set_blog, only: %i[ show edit update destroy, :toggle_status ]
   layout "blog"
-  access all: [:show, :index], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
+  access all: [:show, :index], user: { except: [:destroy, :new, :create, :update, :edit, :toggle_status] }, site_admin: :all
 
   # GET /blogs or /blogs.json
   def index
-    
     @blogs = Blog.special_blogs
-    
+
     @page_title = "My Porfolio Blog"
   end
 
@@ -62,26 +61,28 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
-    
-    def toggle_status
-      if @blog.draft?
-        @blog.published!
-      elseif @blog.published?
-        @blog.draft!
-      end
 
-      redirect_to blogs_url, notice: 'Post status has been updated'
+  def toggle_status
+    if @blog.draft?
+      @blog.published!
+    elsif @blog.published?
+      @blog.draft!
     end
+
+    redirect_to blogs_url, notice: "Post status has been updated."
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_blog
-      @blog = Blog.friendly.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def blog_params
-      params.require(:blog).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_blog
+    @blog = Blog.friendly.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def blog_params
+    params.require(:blog).permit(:title, :body)
+  end
 end
+
+
